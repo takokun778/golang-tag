8a0a7efa823d96474b7cb88dad361c06e871da37
takokun778 <takokun.778@gmail.com>
2022-08-28 16:58:00 +0900

    feat add urfave/cli

--- go.mod
+++ go.mod
@@ -8,14 +8,18 @@ require (
 	github.com/slack-go/slack v0.11.2
 	github.com/uptrace/bun v1.1.7
 	github.com/uptrace/bun/dialect/pgdialect v1.1.7
+	github.com/urfave/cli/v2 v2.11.2
 )
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
 )
--- main.go
+++ main.go
@@ -1,45 +1,79 @@
 package main
 
 import (
-	"context"
 	"log"
+	"os"
 	"tags/client"
 	"tags/database"
 	"tags/model"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
 	log.Println("start app...")
 
-	ctx := context.Background()
-
-	owner := "golang"
-
-	repository := "go"
-
-	channel := "golang-tag"
-
-	src, err := database.SelectAll(ctx, owner, repository)
-	if err != nil {
-		log.Fatal(err)
+	app := &cli.App{
+		Name:  "tag",
+		Usage: "github tags notification app",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "owner",
+				Aliases:  []string{"o"},
+				Required: true,
+				Usage:    "github repository owner",
+			},
+			&cli.StringFlag{
+				Name:     "repository",
+				Aliases:  []string{"r"},
+				Required: true,
+				Usage:    "github repository name",
+			},
+			&cli.StringFlag{
+				Name:     "channel",
+				Aliases:  []string{"c"},
+				Required: true,
+				Usage:    "slack channel name",
+			},
+		},
+		Action: action,
 	}
 
-	dst, err := client.ListTags(ctx, owner, repository)
-	if err != nil {
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func action(ctx *cli.Context) error {
+	owner := ctx.String("owner")
+
+	repository := ctx.String("repository")
+
+	channel := ctx.String("channel")
+
+	src, err := database.SelectAll(ctx.Context, owner, repository)
+	if err != nil {
+		return err
+	}
+
+	dst, err := client.ListTags(ctx.Context, owner, repository)
+	if err != nil {
+		return err
 	}
 
 	tags := model.Take(dst, src)
 
 	if len(tags) != 0 {
-		if err := database.BulkInsert(ctx, tags); err != nil {
-			log.Fatal(err)
+		if err := database.BulkInsert(ctx.Context, tags); err != nil {
+			return err
 		}
 	} else {
 		log.Println("not found new tag")
 	}
 
-	if err := client.PostMessage(ctx, channel, owner, repository, tags); err != nil {
-		log.Fatal(err)
+	if err := client.PostMessage(ctx.Context, channel, owner, repository, tags); err != nil {
+		return err
 	}
+
+	return nil
 }
